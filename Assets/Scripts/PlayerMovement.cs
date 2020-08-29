f59dfee862ba7c2e9d99cca1e77ee06246ba88cf
Anya <ania.leonova@gmail.com>
2020-08-29 16:26:50 -0700

    Created a player that can look around and move
    Look around with mouse and move with WASD keys

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float movementSpeed = 12f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the input from the arrow keys
+        float rightDirection = Input.GetAxis("Horizontal");
+        float forwardDirection = Input.GetAxis("Vertical");
+
+        // Direction that we want to move in based on the input
+        // Takes into account the local coordinates of the player,
+        // i.e. the direction we are facing
+        Vector3 movement = transform.right * rightDirection
+            + transform.forward * forwardDirection;
+
+        Debug.Log(movement);
+
+        controller.SimpleMove(movement * movementSpeed);
+    }
+}
