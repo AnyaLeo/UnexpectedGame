3cd7363e7dd711f61d4b4c71a85e5c5e8ab03870
Anya <ania.leonova@gmail.com>
2020-08-29 21:45:01 -0700

    Have to press E to interact with interactable objects
    A message will be displayed in the console while looking at the interactable object within a certain radius

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,57 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    /** PUBLIC VARIABLES */
     public CharacterController controller;
-
     public float movementSpeed = 12f;
+    public float radiusToInteract = 3;
+
+    /** PRIVATE VARIABLES */
+    private Camera playerCamera;
+    private InteractableObject focus;
+
+    private void Start()
+    {
+        playerCamera = Camera.main;
+    }
 
     // Update is called once per frame
     void Update()
+    {
+        movePlayer();
+        findInteractable();
+
+        // Detect key presses
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            interactWithFocus();
+        }
+    }
+
+    private void interactWithFocus()
+    {
+        if (focus != null)
+        {
+            Debug.Log("Interacting with object " + focus.name);
+        }
+    }
+
+    private void findInteractable()
+    {
+        // Check if we are looking at an interactable object
+        Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, radiusToInteract))
+        {
+            InteractableObject interactable =
+                hit.collider.GetComponent<InteractableObject>();
+
+            focus = interactable;
+        }
+    }
+
+    private void movePlayer()
     {
         // Get the input from the arrow keys
         float rightDirection = Input.GetAxis("Horizontal");
