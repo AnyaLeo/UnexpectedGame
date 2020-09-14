f7bd96a627572fbba3bccf4ed1e36505d4fefeca
Anya <ania.leonova@gmail.com>
2020-09-14 01:55:10 -0700

    bug fixes

---
+++ Assets/DeleteDaemon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteDaemon : MonoBehaviour
+{
+    public VIDE_Assign dialogue;
+    public string newDialogue;
+
+    public void changeDialogue()
+    {
+        dialogue.AssignNew(newDialogue);
+    }
+}
